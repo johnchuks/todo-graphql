efbc225b914846fddde5aa580569b325f1ea732c
Johnbosco Ohia <johnbosco.ohia@andela.com>
2017-10-31 14:02:49 +0100

    fix bug related to resolve function

---
+++ README.md
@@ -0,0 +1,17 @@
+### TODO APPLICATION ###
+
+
+#### INTRODUCTION ####
+
+This is a simple Todo API built with graphene (graphql framework for python) and python flask.
+
+
+#### Getting Started ####
+* Clone the repository with ``` git clone https://github.com/johnchuks/todo-graphql ```
+* create a virtual environment like so ``` virtualenv [name of env]/bin/activate ```
+* Enter into the directory and `npm install` to install the javascript dependencies
+* After installation enter the `todo-flask-app` directory
+* Install all the requirements like so ``` pip install -r requirements.txt ```
+* export your application with ``` FLASK_APP=app.py```
+* In the same terminal run ``` flask run ```
+* Open a new terminal and cd into ``` todo-Graphql ``` and run ```json-server --watch db.json```
--- todo-flask-app/api/schema.py
+++ todo-flask-app/api/schema.py
@@ -9,7 +9,7 @@ class TodoItem(graphene.ObjectType):
     todoId = graphene.Int()
     todo = graphene.Field(lambda: Todo)
 
-    def resolve_todo(self, info, **kwargs):
+    def resolve_todo(self, info):
         """Resolve function to get all todo"""
         response = requests.get('http://localhost:3000/todos/%i' % self.todoId).json()
         data = Todo(id=response['id'], name=response['name'])
@@ -21,7 +21,7 @@ class Todo(graphene.ObjectType):
     name = graphene.String(required=True)
     todo_items = graphene.List(lambda: TodoItem)
 
-    def resolve_todo_items(self, info, **kwargs):
+    def resolve_todo_items(self, info):
         """Resolve function to get todo items for a particular todolist"""
         response = requests.get('http://localhost:3000/todos/%i/todoitems' % self.id)
         data = [TodoItem(id=todo['id'], description=todo['description'], status_done=todo['status_done']) for todo in
@@ -35,14 +35,14 @@ class Query(graphene.ObjectType):
     todo = graphene.Field(Todo, id=graphene.Int())
     todo_item = graphene.Field(TodoItem, id=graphene.Int())
 
-    def resolve_todos(self, info, **kwargs):
+    def resolve_todos(self, info):
         """Resolve function to get all todo lists available"""
         response = requests.get('http://localhost:3000/todos').json()
         data = [Todo(id=todo['id'], name=todo['name']) for todo in response]
         return data
 
 
-    def resolve_todo_items(self, info, **kwargs):
+    def resolve_todo_items(self, info):
         """Resolve function to get all todo items available"""
         response = requests.get('http://localhost:3000/todoitems').json()
         data = [TodoItem(id=todo['id'], description=todo['description'], todoId=todo['todoId'],
@@ -50,14 +50,14 @@ class Query(graphene.ObjectType):
         return data
 
 
-    def resolve_todo(self, info, id, **kwargs):
+    def resolve_todo(self, info, id):
         """Resolve function to get a particular todo"""
         response = requests.get('http://localhost:3000/todos/%i' % id).json()
-        data = Todo(id=response['id'], name=response['name'], todo_items=response['todo_items'])
+        data = Todo(id=response['id'], name=response['name'])
         return data
 
 
-    def resolve_todo_item(self, info, id, **kwargs):
+    def resolve_todo_item(self, info, id):
         """Resolve function to get a todo item"""
         response = requests.get('http://localhost:3000/todoitems/%i' % id).json()
         data = TodoItem(id=response['id'], description=response['description'], status_done=response['status_done'],
